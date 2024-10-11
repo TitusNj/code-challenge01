50df6701b376db09116742280140bbf4a6472cae
Titusnj <titusnj99@gmail.com>
2024-10-11 20:24:54 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-// Your code here
+// Your code 
+function Cute(){
+    var img = document.getElementById('image')
+        img.src = 'assets/Cute.jpeg'
+    var charName =document.getElementById('name')
+        id = "Cute"
+
+
+} 
+function Monkey(){
+    var img = document.getElementById('image')
+        img.src = 'assets/Monkey.jpeg'
+
+} 
+function Zebra(){
+    var img = document.getElementById('image')
+        img.src = 'assets/Zebra.jpeg'
+
+}
+function Lion(){
+    var img = document.getElementById('image')
+        img.src = 'assets/Lion.jpeg'
+
+}
+function Panda(){
+    var img = document.getElementById('image')
+        img.src = 'assets/Panda.jpeg'
+
+} 
